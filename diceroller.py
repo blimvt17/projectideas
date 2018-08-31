b7757a48f8635606921673089da2c3ed30ea3dd2
Ryan <jh3155@gmail.com>
2018-08-31 12:48:00 -0400

    adding todos

--- diceroller.py
+++ diceroller.py
@@ -2,10 +2,14 @@
 #user wants it to roll and print the outcome in %
 from random import randint
 
-
+#TODO: function name should have verb, like roll_dice
+#TODO: function should generate a random number and return it
 def dice():
+    #TODO: print is not closed
     print(randint(1,6)
     
+#TODO: instead of yes/no option, get a number from the user and roll the dice that many times
+#TODO: variable name is not appropriate. it's not an answer
 user_answer = input("Do you want to roll the dice one hundred times? (yes or no)")
 for x in range(100):
     if user_answer == "yes":
