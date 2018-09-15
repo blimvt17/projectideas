222b64027f55bbd2218de6f5d4a783fd21af3128
BJ <Blim.vt17@gmail.com>
2018-09-15 10:40:20 -0400

    deleting elif statements by comparing choice == computerchoice

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -4,15 +4,16 @@ import random
 #making a function called play which tests the conditionals of rps logic. 
 def play(choice, computer_choice):
     
-    display_tie = "It's a tie!"
-    display_win = "You won!"
-    display_loss = "You lose :("
+    display_tie = 'It's a tie!'
+    display_win = 'You won!'
+    display_loss = 'You lose :('
+    error_message = 'Invalid choice'
 
-    rock = "rock"
-    paper = "paper"
-    scissors = "scissors"
+    rock = 'rock'
+    paper = 'paper'
+    scissors = 'scissors'
 
-    if (choice == rock and computer_choice == rock):
+    if choice == computer_choice: 
         return display_tie
     elif (choice == rock and computer_choice == scissors):
         return display_win
@@ -22,24 +23,22 @@ def play(choice, computer_choice):
         return display_win
     elif (choice == paper and computer_choice == scissors):
         return display_loss
-    elif (choice == paper and computer_choice == paper):
-        return display_tie
     elif (choice == scissors and computer_choice == paper):
         return display_win
     elif (choice == scissors and computer_choice == rock):
         return display_loss
     else: 
-        return display_tie
+        return error_message
         
 #added a for loop to running the function so you can specify in code how many times you want to play - simply change the value in the range()
-print("I challenge you to a game of Rock Paper Scissors!")
-keep_playing = "Yes"
+print('I challenge you to a game of Rock Paper Scissors!')
+keep_playing = 'Yes'
 
 while keep_playing == 'Yes':
     
 # for x in range(1):
-    # print("I challenge you to a game of Rock Paper Scissors!")
-    player_choice = input("Please choose rock, paper, scissors by typing the full word\n")
+    # print('I challenge you to a game of Rock Paper Scissors!')
+    player_choice = input('Please choose rock, paper, scissors by typing the full word\n')
     
     #I want to exit program if they don't type the choice correctly 
     valid_choices = ['rock', 'paper', 'scissors']
@@ -51,24 +50,24 @@ while keep_playing == 'Yes':
         computer_choice = random.choice(valid_choices)
         result = play(player_choice, computer_choice)
         print('The computer put out ' + computer_choice + '! ' + result)
-    keep_playing = input("Do you want to keep playing? Enter 'Yes' to keep playing \n")
-print("Goodbye") 
+    keep_playing = input('Do you want to keep playing? Enter 'Yes' to keep playing \n')
+print('Thanks for playing!') 
 #test code that tests rock (player_choice) against the computers_choice
-# print("I challenge you to a game of Rock Paper Scissors")
-# player_choice = "r"
-# computer_choice = "r"
+# print('I challenge you to a game of Rock Paper Scissors')
+# player_choice = 'r'
+# computer_choice = 'r'
 # result = play(player_choice, computer_choice)
 # print(result)
 
-# print("I challenge you to a game of Rock Paper Scissors")
-# player_choice = "r"
-# computer_choice = "p"
+# print('I challenge you to a game of Rock Paper Scissors')
+# player_choice = 'r'
+# computer_choice = 'p'
 # result = play(player_choice, computer_choice)
 # print(result)
 
-# print("I challenge you to a game of Rock Paper Scissors")
-# player_choice = "r"
-# computer_choice = "s"
+# print('I challenge you to a game of Rock Paper Scissors')
+# player_choice = 'r'
+# computer_choice = 's'
 # result = play(player_choice, computer_choice)
 # print(result)
     
