fd8007b670733c94e5dc3fa8976521edfaa53798
Ryan <jh3155@gmail.com>
2018-09-21 16:05:41 -0400

    adding review comments

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,17 +1,23 @@
 #making a palindrome checker
 
+#TODO always define functions and classes first. then start the main business logic
 print('Welcome to the palindrome checker!')
 
+#TODO remember the naming convention. function names usually start with a verb
 #convert string into list (list comprehension)
 def string_to_list(check_string):
     string_list = []
     for letter in check_string:
         string_list.append(letter)
+    #TODO remove parenthesis in return statement
     return (string_list)
 
+#TODO remember the naming convention. function names usually start with a verb
+#TODO this function should expect a string, not a list of characters
 #check by using negative index to check if letters match
 def palindrome_checker(listed_string):
     if listed_string == listed_string[::-1]:
+        #this function should return true/false. this prints should be used by the caller of this function.
         print('It is indeed a palindrome!')
     else: 
         print('It was not a palindrome :(')
@@ -19,6 +25,7 @@ def palindrome_checker(listed_string):
 #ask user for a string to check if it's a palindrome
 check_string = input('Please enter the word that you wish to check\n')
 
+#TODO think about the responsibility. if some company wants to call the palindrom checker, now they need to care about turning string into a list of characters and we don't want that. 
 #calling string_to_list function
 listed_string = string_to_list(check_string)    
 palindrome_checker(listed_string)
