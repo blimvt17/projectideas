30f18a413813e2ed67da1c87ae7af1852efbe3df
BJ <Blim.vt17@gmail.com>
2018-10-04 16:16:06 -0400

    revising code in highest_number_finder.py

--- find_highest_number.py
+++ find_highest_number.py
@@ -2,12 +2,13 @@
 #output the highest number 
     
 
-def number_splitter(user_given_numbers):
-    return list(map(int, user_given_numbers.split(', ')))
+# def number_splitter(user_given_numbers):
+#     return list(map(int, user_given_numbers.split(', ')))
 
 
 def highest_number_finder(user_given_numbers):
-    listed_numbers = number_splitter(user_given_numbers)
+    # listed_numbers = number_splitter(user_given_numbers)
+    listed_numbers = [int(x) for x in user_given_numbers().split()]
     max_number = None
     max_number = max(listed_numbers)
     return max_number
@@ -16,4 +17,4 @@ def highest_number_finder(user_given_numbers):
 #ask user for how many numbers to put in list 
 user_given_numbers = (input('Please enter the number in the following format: #, #, #, #, etc..'))
 found_max_number = highest_number_finder(user_given_numbers)
-print('The maximum number found was: ' + str(found_max_number))
+print(found_max_number)
